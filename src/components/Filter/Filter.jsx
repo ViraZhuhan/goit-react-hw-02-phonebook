13d37a01857e1cfa199735cf272c76605296fda6
Vira Zhuhan <115663875+ViraZhuhan@users.noreply.github.com>
2023-05-31 16:55:46 +0200

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,23 +1,18 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {Label, Input} from './Filter.styled'
 
-class Filter extends Component {
-  static propTypes = {
-    onChange: PropTypes.func.isRequired,
-    value: PropTypes.string.isRequired,
-  };
-
-  render() {
-    const { onChange, value } = this.props;
-
-    return (
+const Filter = ({onChange, value}) => {
+  return (
       <>
         <Label>Find contacts by name</Label>
         <Input type="text" name="filter" onChange={onChange} value={value} />
       </>
     );
-  }
 }
+
+Filter.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+};
 
 export default Filter;
